7365c2f2531a244df84a6cc35b946e16a67657d7
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-05-01 12:42:10 +0530

    Create customdropdown.dart

---
+++ Projects/lib/widgets/customdropdown.dart
@@ -0,0 +1,31 @@
+
+
+import 'package:flutter/material.dart';
+
+class CustomDropdown<T> extends StatelessWidget {
+  final String label;
+  final T? value;
+  final List<DropdownMenuItem<T>> items;
+  final Function(T?) onChanged;
+  final String? Function(T?)? validator;
+
+  const CustomDropdown({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.items,
+    required this.onChanged,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<T>(
+      value: value,
+      decoration: InputDecoration(labelText: label),
+      items: items,
+      onChanged: onChanged,
+      validator: validator,
+    );
+  }
+}
