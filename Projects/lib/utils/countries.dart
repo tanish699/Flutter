f9578267d6298e2f4c4b2d9db7778d956b0be238
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-05-01 12:30:12 +0530

    Create countries.dart

---
+++ Projects/lib/utils/countries.dart
@@ -0,0 +1,21 @@
+
+
+
+
+
+class Country{
+  final String name;
+
+
+  Country({required this.name});
+
+
+}
+
+List<Country> countryList = [
+  Country(name:"India"),
+  Country(name: "USA"),
+  Country(name: "UK"),
+  Country(name: "Canada"),
+  Country(name: "Australia"),
+];
