b70051b8665470bb6d66e1280b15d8e2714db5d4
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-05-04 12:43:29 +0530

    started new project

---
+++ traning_task_2/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+// import 'package:registration_flow/pages/login.dart';
+// import 'package:registration_flow/pages/profile.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+import '../utils/images.dart';
+// import '../widgets/custombutton.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // final prefs = await SharedPreferences.getInstance();
+  // final email = prefs.getString("email");
+
+
+  // runApp(MyApp(isLoggedIn: email != null));
+}
+
+class MyApp extends StatelessWidget {
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // return MaterialApp(
+    //   home:  isLoggedIn ? const userProfile() : LandingPage(),
+    // );
+    return MaterialApp(home: LandingPage());
+  }
+}
+
+class LandingPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+
+      // Nav bar
+      appBar: AppBar(
+          title: Text("Greetings Developer"),
+          bottom: PreferredSize(preferredSize: Size.fromHeight(1), child: Divider(height: 1,thickness: 1,))
+      ),
+
+      // main Screen
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Image.asset(AppImages.welcome,width: 400,height: 400, fit: BoxFit.contain,),
+                SizedBox(height: 20,),
+                Text("We are excited to have you here!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22), textAlign: TextAlign.center,),
+                SizedBox(height: 10,),
+                // Text("We're excited to have you here!", textAlign: TextAlign.center, style: TextStyle(fontSize: 18),),
+                SizedBox(height: 10,),
+                Text("This app is crafted to teach you the fundamentals and get you comfortable with building mobile applications using Flutter.!", textAlign: TextAlign.center,style: TextStyle(fontSize: 16, color: Colors.grey), ),
+                SizedBox(height: 30,),
+
+                //Button
+                // Align(
+                //   alignment: Alignment.centerRight,
+                //   child: CustomButton(
+                //     text: "Continue →",
+                //     width: 150,
+                //     onTap: () {
+                //       Navigator.push(
+                //         context,
+                //         MaterialPageRoute(
+                //           builder: (context) => loginPage(),
+                //         ),
+                //       );
+                //     },
+                //   ),
+                // ),
+                SizedBox(height: 20),
+              ],
+            ),
+
+          ),
+
+        ),
+
+      ),
+    );
+
+  }
+}
---
+++ traning_task_2/lib/utils/images.dart
@@ -0,0 +1,14 @@
+
+
+class AppImages{
+
+  static const String welcome = "assets/images/welcome_background.png";
+  static const String registrationHeader = "assets/images/register_background.png";
+  static const String appicon = "assets/images/Appicon.png";
+  static const String contact = "assets/images/contact.png";
+  static const String loginbg = "assets/images/login_background.png";
+  static const String settings = "assets/images/settings.png";
+  static const String user = "assets/images/user_logo.png";
+  static const String visit = "assets/images/visit.png";
+
+}
